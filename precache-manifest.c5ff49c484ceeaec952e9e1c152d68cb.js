ccdc6169e1fec0604c86cc2a9b7582100c749834
vargas_gabriela <gvargas@icov.cl>
2020-02-05 12:59:18 -0300

    Updates

---
+++ precache-manifest.c5ff49c484ceeaec952e9e1c152d68cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39047a24449942b7848956b8e346caf6",
+    "url": "/react-tutorial-deploy/index.html"
+  },
+  {
+    "revision": "85424d85b56de2713294",
+    "url": "/react-tutorial-deploy/static/css/main.a436cb7d.chunk.css"
+  },
+  {
+    "revision": "e257cc4ac4564e43f115",
+    "url": "/react-tutorial-deploy/static/js/2.29478003.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tutorial-deploy/static/js/2.29478003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85424d85b56de2713294",
+    "url": "/react-tutorial-deploy/static/js/main.1ecac012.chunk.js"
+  },
+  {
+    "revision": "5f15725ad51b89b374a2",
+    "url": "/react-tutorial-deploy/static/js/runtime-main.6e7e056a.js"
+  }
+]);
